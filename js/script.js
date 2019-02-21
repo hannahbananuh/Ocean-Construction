53ec4228b24a8ad434f0ad0a4e8e2500203de48d
Hannah Alcantara <hannah@Hannahs-MBP.socal.rr.com>
2019-02-21 00:50:17 -0800

    Initialized Ocean Construction Website

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function () {
+    $('.parallax').parallax();
+    $('.slider').slider({
+        height: 550,
+        interval: 5000,
+        duration: 400
+    });
+    $('.modal').modal();
+});
+
+
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true
+});
+
+$(document).ready(function(){
+    $('.parallax').parallax();
+  });
+        
